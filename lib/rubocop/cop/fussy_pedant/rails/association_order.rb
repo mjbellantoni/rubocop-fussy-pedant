09bcf3b25b76098b528634470a690a1f477d8e19
Matthew Bellantoni <mjbellantoni@gmail.com>
2026-03-03 10:29:28 -0500

    Add type ordering detection to AssociationOrder cop

--- lib/rubocop/cop/fussy_pedant/rails/association_order.rb
+++ lib/rubocop/cop/fussy_pedant/rails/association_order.rb
@@ -44,8 +44,86 @@ module RuboCop
             belongs_to has_one has_many has_and_belongs_to_many
           ].freeze
 
+          MSG_TYPE_ORDER = 'Expected `%<type>s :%<name>s` to come after ' \
+                           '`%<prev_type>s` associations, not before.'
+
           def on_class(node)
-            # TODO: implement
+            associations = collect_associations(node)
+            return if associations.size < 2
+
+            check_type_ordering(associations)
+          end
+
+          private
+
+          def collect_associations(class_node)
+            associations = []
+            class_node.body&.each_child_node do |child|
+              next unless child.send_type?
+              next unless ASSOCIATION_METHODS.include?(child.method_name)
+
+              associations << build_association(child)
+            end
+            associations
+          end
+
+          def build_association(node)
+            {
+              node: node,
+              method_name: node.method_name,
+              name: association_name(node),
+              through: through_association?(node),
+              rank: compute_rank(node)
+            }
+          end
+
+          def association_name(node)
+            node.first_argument.value
+          end
+
+          def through_association?(node)
+            node.arguments.any? do |arg|
+              next unless arg.hash_type?
+
+              arg.pairs.any? { |pair| pair.key.value == :through }
+            end
+          end
+
+          def compute_rank(node)
+            through = through_association?(node)
+            case node.method_name
+            when :belongs_to then 0
+            when :has_one then through ? 2 : 1
+            when :has_many then through ? 4 : 3
+            when :has_and_belongs_to_many then 5
+            end
+          end
+
+          def subtype_label(association)
+            method = association[:method_name]
+            through = association[:through]
+            case method
+            when :belongs_to then 'belongs_to'
+            when :has_one then through ? 'has_one :through' : 'has_one'
+            when :has_many then through ? 'has_many :through' : 'has_many'
+            when :has_and_belongs_to_many then 'has_and_belongs_to_many'
+            end
+          end
+
+          def check_type_ordering(associations)
+            associations.each_cons(2) do |prev_assoc, curr_assoc|
+              next if curr_assoc[:rank] >= prev_assoc[:rank]
+
+              add_offense(curr_assoc[:node],
+                          message: type_order_message(curr_assoc, prev_assoc))
+            end
+          end
+
+          def type_order_message(curr_assoc, prev_assoc)
+            format(MSG_TYPE_ORDER,
+                   type: subtype_label(curr_assoc),
+                   name: curr_assoc[:name],
+                   prev_type: subtype_label(prev_assoc))
           end
         end
       end
--- spec/rubocop/cop/fussy_pedant/rails/association_order_spec.rb
+++ spec/rubocop/cop/fussy_pedant/rails/association_order_spec.rb
@@ -20,4 +20,87 @@ RSpec.describe RuboCop::Cop::FussyPedant::Rails::AssociationOrder, :config do
       RUBY
     end
   end
+
+  context 'with type ordering' do
+    it 'does not register an offense when associations are in correct type order' do
+      expect_no_offenses(<<~RUBY)
+        class User < ApplicationRecord
+          belongs_to :company
+          has_many :posts
+        end
+      RUBY
+    end
+
+    it 'registers an offense when belongs_to comes after has_many' do
+      expect_offense(<<~RUBY)
+        class User < ApplicationRecord
+          has_many :posts
+          belongs_to :company
+          ^^^^^^^^^^^^^^^^^^^ FussyPedant/Rails/AssociationOrder: Expected `belongs_to :company` to come after `has_many` associations, not before.
+        end
+      RUBY
+    end
+
+    it 'does not register an offense for full correct type order' do
+      expect_no_offenses(<<~RUBY)
+        class Post < ApplicationRecord
+          belongs_to :author
+          has_one :metadata
+          has_many :comments
+          has_and_belongs_to_many :tags
+        end
+      RUBY
+    end
+
+    it 'registers an offense when has_one comes after has_many' do
+      expect_offense(<<~RUBY)
+        class Post < ApplicationRecord
+          has_many :comments
+          has_one :metadata
+          ^^^^^^^^^^^^^^^^^ FussyPedant/Rails/AssociationOrder: Expected `has_one :metadata` to come after `has_many` associations, not before.
+        end
+      RUBY
+    end
+
+    it 'handles through variants as separate subtypes' do
+      expect_no_offenses(<<~RUBY)
+        class User < ApplicationRecord
+          has_one :profile
+          has_one :avatar, through: :profile
+          has_many :posts
+          has_many :comments, through: :posts
+        end
+      RUBY
+    end
+
+    it 'registers an offense when has_many comes after has_many :through' do
+      expect_offense(<<~RUBY)
+        class User < ApplicationRecord
+          has_many :comments, through: :posts
+          has_many :posts
+          ^^^^^^^^^^^^^^^ FussyPedant/Rails/AssociationOrder: Expected `has_many :posts` to come after `has_many :through` associations, not before.
+        end
+      RUBY
+    end
+
+    it 'registers an offense when has_one :through comes before has_one' do
+      expect_offense(<<~RUBY)
+        class User < ApplicationRecord
+          has_one :avatar, through: :profile
+          has_one :profile
+          ^^^^^^^^^^^^^^^^ FussyPedant/Rails/AssociationOrder: Expected `has_one :profile` to come after `has_one :through` associations, not before.
+        end
+      RUBY
+    end
+
+    it 'ignores non-association methods between associations' do
+      expect_no_offenses(<<~RUBY)
+        class User < ApplicationRecord
+          belongs_to :company
+          validates :name, presence: true
+          has_many :posts
+        end
+      RUBY
+    end
+  end
 end
