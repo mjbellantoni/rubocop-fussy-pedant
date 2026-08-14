b5bbde80570dea5e8c0ccaf1d422581a4b21fe52
Matthew Bellantoni <mjbellantoni@gmail.com>
2026-08-14 16:33:01 -0400

    Add SpaceInsidePercentArray cop

---
+++ spec/rubocop/cop/fussy_pedant/ruby/space_inside_percent_array_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::FussyPedant::Ruby::SpaceInsidePercentArray, :config do
+  let(:config) { RuboCop::Config.new }
+
+  it 'registers an offense and corrects a %w array without inner spaces' do
+    expect_offense(<<~RUBY)
+      %w[foo bar]
+      ^^^^^^^^^^^ FussyPedant/Ruby/SpaceInsidePercentArray: Put a space inside the delimiters of a percent array.
+    RUBY
+    expect_correction(<<~RUBY)
+      %w[ foo bar ]
+    RUBY
+  end
+
+  it 'registers an offense when only the leading space is missing' do
+    expect_offense(<<~RUBY)
+      %i[foo ]
+      ^^^^^^^^ FussyPedant/Ruby/SpaceInsidePercentArray: Put a space inside the delimiters of a percent array.
+    RUBY
+    expect_correction(<<~RUBY)
+      %i[ foo ]
+    RUBY
+  end
+
+  it 'accepts a correctly spaced array' do
+    expect_no_offenses('%w[ foo bar ]')
+  end
+
+  it 'accepts an empty percent array' do
+    expect_no_offenses('%w[]')
+  end
+
+  it 'ignores non-array percent literals' do
+    expect_no_offenses('%q(foo)')
+  end
+
+  it 'handles other delimiters' do
+    expect_offense(<<~RUBY)
+      %w(foo)
+      ^^^^^^^ FussyPedant/Ruby/SpaceInsidePercentArray: Put a space inside the delimiters of a percent array.
+    RUBY
+    expect_correction(<<~RUBY)
+      %w( foo )
+    RUBY
+  end
+end
