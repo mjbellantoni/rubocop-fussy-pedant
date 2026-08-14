4ff0e61c5e573fbdceca3c04d28e11a9cc4971ae
Matthew Bellantoni <mjbellantoni@gmail.com>
2026-08-14 16:41:12 -0400

    Skip multiline arrays in SpaceInsidePercentArray

--- lib/rubocop/cop/fussy_pedant/ruby/space_inside_percent_array.rb
+++ lib/rubocop/cop/fussy_pedant/ruby/space_inside_percent_array.rb
@@ -23,6 +23,7 @@ module RuboCop
           def on_array(node)
             return unless percent_array?(node)
             return if node.children.empty?
+            return if node.multiline?
 
             source = node.source
             open_index = source.index(/[\[({<]/)
@@ -31,12 +32,16 @@ module RuboCop
             inner = source[(open_index + 1)...-1]
             return if inner.start_with?(' ') && inner.end_with?(' ')
 
+            register_offense(node, source, open_index, inner)
+          end
+
+          private
+
+          def register_offense(node, source, open_index, inner)
             add_offense(node) do |corrector|
               corrector.replace(node, corrected(source, open_index, inner))
             end
           end
-
-          private
 
           def percent_array?(node)
             node.source.match?(PERCENT_ARRAY)
@@ -45,6 +50,8 @@ module RuboCop
           def corrected(source, open_index, inner)
             prefix = source[0..open_index]
             closer = source[-1]
+            # strip intentionally normalizes any interior leading/trailing
+            # whitespace (tabs included) down to a single space on each side.
             "#{prefix} #{inner.strip} #{closer}"
           end
         end
--- spec/rubocop/cop/fussy_pedant/ruby/space_inside_percent_array_spec.rb
+++ spec/rubocop/cop/fussy_pedant/ruby/space_inside_percent_array_spec.rb
@@ -44,4 +44,33 @@ RSpec.describe RuboCop::Cop::FussyPedant::Ruby::SpaceInsidePercentArray, :config
       %w( foo )
     RUBY
   end
+
+  it 'leaves a multiline %w array untouched' do
+    expect_no_offenses(<<~RUBY)
+      %w[
+        foo
+        bar
+      ]
+    RUBY
+  end
+
+  it 'corrects a %W array with interpolation' do
+    expect_offense(<<~'RUBY')
+      %W[foo#{bar}]
+      ^^^^^^^^^^^^^ FussyPedant/Ruby/SpaceInsidePercentArray: Put a space inside the delimiters of a percent array.
+    RUBY
+    expect_correction(<<~'RUBY')
+      %W[ foo#{bar} ]
+    RUBY
+  end
+
+  it 'corrects a brace-delimited array' do
+    expect_offense(<<~RUBY)
+      %i{foo}
+      ^^^^^^^ FussyPedant/Ruby/SpaceInsidePercentArray: Put a space inside the delimiters of a percent array.
+    RUBY
+    expect_correction(<<~RUBY)
+      %i{ foo }
+    RUBY
+  end
 end
